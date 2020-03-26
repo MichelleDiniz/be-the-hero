3a982e3132ba915fd54a410b0df89e9dea65a947
MichelleDiniz <mfabianad@gmail.com>
2020-03-26 20:56:37 -0300

    Connect new incident form

--- frontend/src/pages/newIncident/index.js
+++ frontend/src/pages/newIncident/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+  const history = useHistory();
+  const [ title, setTitle] = useState('');
+  const [ description, setDescription] = useState('');
+  const [ value, setvalue] = useState('');
+
+  async function handleNewIncident(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value
+    }
+
+    try {
+      await api.post('incidents', data, {
+        headers: {
+          Authorization: localStorage.getItem('ongId'),
+        }
+      });
+
+      history.push('/profile');
+    } catch (err) {
+
+    }
+  }
+
   return(
     <div className="new-incident-container">
       <div className="new-incident-content">
@@ -22,10 +51,22 @@ export default function NewIncident() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Titulo do caso" />
-          <textarea placeholder="Descrição"></textarea>
-          <input placeholder="Valor em reais" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            placeholder="Titulo do caso"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+          ></textarea>
+          <input
+            placeholder="Valor em reais"
+            value={value}
+            onChange={e => setvalue(e.target.value)}
+          />
 
           <button className="button" type="submit">Cadastrar</button>
         </form>
